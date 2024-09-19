944e5a6ebe3a656939372c16a86f200c452a6f84
GabrielSL24 <g.soto.1@estudiantec.cr>
2024-09-18 23:48:35 -0600

    new commit

---
+++ TinySQLD-B/ApiInterface/Server.cs
@@ -0,0 +1,82 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using ApiInterface.InternalModels;
+using System.Text.Json;
+using ApiInterface.Exceptions;
+using ApiInterface.Processors;
+using ApiInterface.Models;
+
+namespace ApiInterface
+{
+    public class Server
+    {
+        private static IPEndPoint serverEndPoint = new(IPAddress.Loopback, 11000);
+        private static int supportedParallelConnections = 1;
+
+        public static async Task Start()
+        {
+            using Socket listener = new(serverEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(serverEndPoint);
+            listener.Listen(supportedParallelConnections);
+            Console.WriteLine($"Server ready at {serverEndPoint.ToString()}");
+
+            while (true)
+            {
+                var handler = await listener.AcceptAsync();
+                try
+                {
+                    var rawMessage = GetMessage(handler);
+                    var requestObject = ConvertToRequestObject(rawMessage);
+                    var response = ProcessRequest(requestObject);
+                    SendResponse(response, handler);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    await SendErrorResponse("Unknown exception", handler);
+                }
+                finally
+                {
+                    handler.Close();
+                }
+            }
+        }
+
+        private static string GetMessage(Socket handler)
+        {
+            using (NetworkStream stream = new NetworkStream(handler))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadLine() ?? String.Empty;
+            }
+        }
+
+        private static Request ConvertToRequestObject(string rawMessage)
+        {
+            return JsonSerializer.Deserialize<Request>(rawMessage) ?? throw new InvalidRequestException();
+        }
+
+        private static Response ProcessRequest(Request requestObject)
+        {
+            var processor = ProcessorFactory.Create(requestObject);
+            return processor.Process();
+        }
+
+        private static void SendResponse(Response response, Socket handler)
+        {
+            using (NetworkStream stream = new NetworkStream(handler))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine(JsonSerializer.Serialize(response));
+            }
+        }
+
+        private static Task SendErrorResponse(string reason, Socket handler)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ TinySQLD-B/Client/tinysqlclient.ps1
@@ -0,0 +1,69 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$IP,
+    [Parameter(Mandatory = $true)]
+    [int]$Port
+)
+
+$ipEndPoint = [System.Net.IPEndPoint]::new([System.Net.IPAddress]::Parse("127.0.0.1"), 11000)
+
+function Send-Message {
+    param (
+        [Parameter(Mandatory=$true)]
+        [pscustomobject]$message,
+        [Parameter(Mandatory=$true)]
+        [System.Net.Sockets.Socket]$client
+    )
+
+    $stream = New-Object System.Net.Sockets.NetworkStream($client)
+    $writer = New-Object System.IO.StreamWriter($stream)
+    try {
+        $writer.WriteLine($message)
+    }
+    finally {
+        $writer.Close()
+        $stream.Close()
+    }
+}
+
+function Receive-Message {
+    param (
+        [System.Net.Sockets.Socket]$client
+    )
+    $stream = New-Object System.Net.Sockets.NetworkStream($client)
+    $reader = New-Object System.IO.StreamReader($stream)
+    try {
+        return $null -ne $reader.ReadLine ? $reader.ReadLine() : ""
+    }
+    finally {
+        $reader.Close()
+        $stream.Close()
+    }
+}
+function Send-SQLCommand {
+    param (
+        [string]$command
+    )
+    $client = New-Object System.Net.Sockets.Socket($ipEndPoint.AddressFamily, [System.Net.Sockets.SocketType]::Stream, [System.Net.Sockets.ProtocolType]::Tcp)
+    $client.Connect($ipEndPoint)
+    $requestObject = [PSCustomObject]@{
+        RequestType = 0;
+        RequestBody = $command
+    }
+    Write-Host -ForegroundColor Green "Sending command: $command"
+
+    $jsonMessage = ConvertTo-Json -InputObject $requestObject -Compress
+    Send-Message -client $client -message $jsonMessage
+    $response = Receive-Message -client $client
+
+    Write-Host -ForegroundColor Green "Response received: $response"
+    
+    $responseObject = ConvertFrom-Json -InputObject $response
+    Write-Output $responseObject
+    $client.Shutdown([System.Net.Sockets.SocketShutdown]::Both)
+    $client.Close()
+}
+
+# This is an example, should not be called here
+Send-SQLCommand -command "CREATE TABLE ESTUDIANTE"
+Send-SQlCommand -command "SELECT * FROM ESTUDIANTE"
