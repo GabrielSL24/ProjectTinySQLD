4735512612e1ebe3b10500fdc21b43ba86f4bc9b
SebKoreano <99543845+SebKoreano@users.noreply.github.com>
2024-09-29 22:02:46 -0600

    Modularize client
    Se modularizo el PowerShell del cliente

--- TinySQLD-B/Client/tinysqlclient.ps1
+++ TinySQLD-B/Client/tinysqlclient.ps1
@@ -8,91 +8,136 @@ function Execute-MyQuery {
         [string]$IP  
     )
 
+    # Validar el archivo
+    validateFile -QueryFile $QueryFile
+
+    # Crear el EndPoint
+    $ipEndPoint = Get-EndPoint -IP $IP -Port $Port
+
+    # Leer y procesar el archivo
+    Process-QueryFile -QueryFile $QueryFile -ipEndPoint $ipEndPoint -Port $Port -IP $IP
+}
+
+function validateFile {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$QueryFile 
+    )
+
     if ([System.IO.Path]::GetExtension($QueryFile) -ne ".tinysql") {
-        Write-Host -ForegroundColor Red "Error: El archivo debe tener la extension .tinysql."
+        Write-Host -ForegroundColor Red "Error: El archivo debe tener la extensión .tinysql."
         exit 1
     }
 
-    # Verificar si el archivo existe
     if (-not (Test-Path $QueryFile)) {
         Write-Host -ForegroundColor Red "Error: El archivo $QueryFile no existe. Verifique la ruta."
         exit 1
     }
+}
 
-    $ipEndPoint = [System.Net.IPEndPoint]::new([System.Net.IPAddress]::Parse($IP), $Port)
-
-    function Send-Message {
-        param (
-            [Parameter(Mandatory=$true)]
-            [pscustomobject]$message,
-            [Parameter(Mandatory=$true)]
-            [System.Net.Sockets.Socket]$client
-        )
-
-        $stream = New-Object System.Net.Sockets.NetworkStream($client)
-        $writer = New-Object System.IO.StreamWriter($stream)
-        try {
-            $writer.WriteLine($message)
-        }
-        finally {
-            $writer.Close()
-            $stream.Close()
-        }
+function Get-EndPoint {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$IP,
+        [Parameter(Mandatory = $true)]
+        [int]$Port
+    )
+    
+    try {
+        $ipEndPoint = [System.Net.IPEndPoint]::new([System.Net.IPAddress]::Parse($IP), $Port)
+        return $ipEndPoint
+    } catch {
+        Write-Host -ForegroundColor Red "Error: La IP o el puerto son incorrectos."
+        exit 1
     }
+}
 
-    function Receive-Message {
-        param (
-            [System.Net.Sockets.Socket]$client
-        )
-        $stream = New-Object System.Net.Sockets.NetworkStream($client)
-        $reader = New-Object System.IO.StreamReader($stream)
-        try {
-            return $null -ne $reader.ReadLine ? $reader.ReadLine() : ""
-        }
-        finally {
-            $reader.Close()
-            $stream.Close()
-        }
-    }
+function Process-QueryFile {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$QueryFile,
+        [System.Net.IPEndPoint]$ipEndPoint,
+        [int]$Port,  
+        [string]$IP  
+    )
 
-    function Send-SQLCommand {
-        param (
-            [string]$command
-        )
-        $client = New-Object System.Net.Sockets.Socket($ipEndPoint.AddressFamily, [System.Net.Sockets.SocketType]::Stream, [System.Net.Sockets.ProtocolType]::Tcp)
-        
-        try {
-            $client.Connect($ipEndPoint)
-        } catch {
-            Write-Host -ForegroundColor Red "Error: No se pudo conectar al servidor en $IP :$Port. Verifique que la direccion IP y el puerto sean correctos, o si el servidor esta activo."
-            exit 1
-        }
-        
-        $requestObject = [PSCustomObject]@{
-            RequestType = 0;
-            RequestBody = $command
-        }
-        Write-Host -ForegroundColor Green "Enviando comando: $command"
-
-        $jsonMessage = ConvertTo-Json -InputObject $requestObject -Compress
-        Send-Message -client $client -message $jsonMessage
-        $response = Receive-Message -client $client
-
-        Write-Host -ForegroundColor Green "Respuesta recibida: $response"
-        
-        $responseObject = ConvertFrom-Json -InputObject $response
-        Write-Output $responseObject
-        $client.Shutdown([System.Net.Sockets.SocketShutdown]::Both)
-        $client.Close()
-    }
-
-    #Leer el archivo
     Write-Host -ForegroundColor Yellow "Leyendo el archivo de instrucciones: $QueryFile"
     $lines = Get-Content $QueryFile
     foreach ($line in $lines) {
         if (-not [string]::IsNullOrWhiteSpace($line)) {
-            Send-SQLCommand -command $line
+            Send-SQLCommand -command $line -ipEndPoint $ipEndPoint -Port $Port -IP $IP
         }
     }
+}
 
+function Send-Message {
+    param (
+        [Parameter(Mandatory=$true)]
+        [pscustomobject]$message,
+        [Parameter(Mandatory=$true)]
+        [System.Net.Sockets.Socket]$client
+    )
+
+    $stream = New-Object System.Net.Sockets.NetworkStream($client)
+    $writer = New-Object System.IO.StreamWriter($stream)
+    try {
+        $writer.WriteLine($message)
+    }
+    finally {
+        $writer.Close()
+        $stream.Close()
+    }
+}
+
+function Receive-Message {
+    param (
+        [System.Net.Sockets.Socket]$client
+    )
+
+    $stream = New-Object System.Net.Sockets.NetworkStream($client)
+    $reader = New-Object System.IO.StreamReader($stream)
+    try {
+        return $null -ne $reader.ReadLine ? $reader.ReadLine() : ""
+    }
+    finally {
+        $reader.Close()
+        $stream.Close()
+    }
+}
+
+function Send-SQLCommand {
+    param (
+        [string]$command,
+        [System.Net.IPEndPoint]$ipEndPoint,
+        [int]$Port,  
+        [string]$IP  
+    )
+
+    $client = New-Object System.Net.Sockets.Socket($ipEndPoint.AddressFamily, [System.Net.Sockets.SocketType]::Stream, [System.Net.Sockets.ProtocolType]::Tcp)
+
+    try {
+        $client.Connect($ipEndPoint)
+    } catch {
+        Write-Host -ForegroundColor Red "Error: No se pudo conectar al servidor en $IP :$Port. Verifique la dirección IP y el puerto, o si el servidor está activo."
+        exit 1
+    }
+    
+    $requestObject = [PSCustomObject]@{
+        RequestType = 0;
+        RequestBody = $command
+    }
+
+    Write-Host -ForegroundColor Green "Enviando comando: $command"
+
+    $jsonMessage = ConvertTo-Json -InputObject $requestObject -Compress
+    Send-Message -client $client -message $jsonMessage
+
+    $response = Receive-Message -client $client
+    Write-Host -ForegroundColor Green "Respuesta recibida: $response"
+
+    $responseObject = ConvertFrom-Json -InputObject $response
+    Write-Output $responseObject
+
+    $client.Shutdown([System.Net.Sockets.SocketShutdown]::Both)
+    $client.Close()
 }
